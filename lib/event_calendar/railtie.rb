5a3938bb3ec5883eb559032445ecabca0aacff15
KalabiYau <skullzeek@gmail.com>
2013-03-12 00:37:57 +0400

    Replace engine with railtie

--- lib/event_calendar/railtie.rb
+++ lib/event_calendar/railtie.rb
@@ -1,19 +1,11 @@
-require 'event_calendar'
 require 'event_calendar/calendar_helper'
-require 'rails'
 
 module EventCalendar
-  class Railtie < Rails::Engine
-    initializer :after_initialize do
-      if defined?(ActionController::Base)
-        ActionController::Base.helper EventCalendar::CalendarHelper
-      end
-      if defined?(ActiveRecord::Base)
-        ActiveRecord::Base.extend EventCalendar::ClassMethods
-      end
+  class Railtie < Rails::Railtie
+    initializer 'event_calendar.view_helpers' do
+      ActionView::Base.send :include, CalendarHelper
     end
   end
 end
 
-# Support other ORMs
 require 'event_calendar/orm/mongoid' if defined? Mongoid
